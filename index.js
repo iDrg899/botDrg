a07b3d62a76b9e2b20fb9c76935d7037c37aef1f
varghs <svarghs8531@gmail.com>
2023-06-20 15:45:58 -0400

    did the deck and card stuff

--- index.js
+++ index.js
@@ -27,6 +27,54 @@ for (const file of commandFiles) {
     client.commands.set(command.name, command);
 }
 
+const SUITS = ["S", "H", "D", "C"];
+const VALUES = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+/* Card class */
+class Card {
+  constructor(suit, value) {
+    if (SUITS.includes(suit) || suit === "J") {
+      if (VALUES.includes(value) || (suit === "J" && (value === "B" || value === "R"))) {
+        this.suit = suit;
+        this.value = value;
+      } else {
+        console.log("Error! Value bad!");
+      }
+    } else {
+      console.log("Error! Suit bad!");
+    }
+  }
+}
+
+/* Deck class */
+class Deck {
+  constructor() {
+    let cards = [];
+    for (let i = 0; i < SUITS.length; i++) {
+      for (let j = 0; j < VALUES.length; j++) {
+        cards.push(new Card(SUITS[i], VALUES[j]));
+      }
+    }
+    cards.push(new Card("J", "B"));
+    cards.push(new Card("J", "R"));
+    this.cards = cards;
+  }
+
+  shuffle() {
+    for (let i = this.cards.length-1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i+1));
+
+      let tmp = this.cards[i];
+      this.cards[i] = this.cards[j];
+      this.cards[j] = tmp;
+    }
+  }
+}
+
+let deck = new Deck();
+deck.shuffle();
+console.log(deck.cards);
+
 
 // Beginning of Fish BS
 class Fish {
