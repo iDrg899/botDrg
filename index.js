b5e5b11c8569eb0d4690ef1769edf8f091d2f7c5
varghs <svarghs8531@gmail.com>
2023-06-29 17:31:47 -0400

    Revert "fjkldsa"
    This reverts commit 24306b3ba348314d3c1e8f3ad61085211f2cd14c.

--- index.js
+++ index.js
@@ -834,8 +834,7 @@ client.on('interactionCreate', async (interaction) => {
     case "cards":
       let p = fishGame.getPlayerFromId(interaction.user.id);
       let hand = p.hand;
-      console.log(p.hand);
-      interaction.reply({files: [await showHandPNG(hand)], ephemeral:true});
+      interaction.reply({files: [showHandPNG(hand)], ephemeral:true});
       break;
   }
 });
